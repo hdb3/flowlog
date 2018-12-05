a2aeb16b901a0057e443e3f6933981dc1db893ac
Nicholas Hart <nic@hdb3.net>
2018-12-05 07:36:46 +0000

    WiP

--- Flowlog.hs
+++ Flowlog.hs
@@ -2,9 +2,10 @@ module Main where
 
 import qualified Data.Map.Strict
 import qualified Data.IP
+import Data.Maybe(isJust,fromMaybe)
 
-data Flow = Flow { tOpen , tFirstUpdate, tLastUpdate, tNotify :: Maybe Double } deriving Show
-nullFlow = Flow Nothing Nothing Nothing Nothing
+data Flow = Flow { tOpen , tFirstUpdate, tLastUpdate, tNotify :: Maybe Double, updateCount, openCount :: Int } deriving Show
+nullFlow = Flow Nothing Nothing Nothing Nothing 0 0
 type LogMap = Data.Map.Strict.Map (Data.IP.IPv4,Data.IP.IPv4) Flow
 
 main = do
@@ -13,6 +14,13 @@ main = do
         flowMap = foldl processTrace newMap (lines logs)
         flowList = map readTraceLine (lines logs)
     putStrLn $ unlines $ map show $ Data.Map.Strict.toList flowMap
+
+displayFlows flows = map displayFlow $ filter (isJust . tFirstUpdate . snd) flows
+
+displayFlow ((src,dst),( Flow tOpen tFirstUpdate tLastUpdate tNotify updateCount openCount )) =
+    show src ++ " -> " ++ show dst ++ show (tLastUpdate-tFirstUpdate)
+         ++ " " ++ show updateCount ++ " " ++ show openCount ++ " "
+         ++ ( maybe "0" (\t -> show (fromMaybe tFirstUpdate - t) tOpen ))
 
 processTrace flowMap logLine = Data.Map.Strict.alter (f ts types) (src,dst) flowMap where
     vals@(ts,src,dst,types) = readTraceLine logLine
@@ -23,14 +31,14 @@ processTrace flowMap logLine = Data.Map.Strict.alter (f ts types) (src,dst) flow
 
     -- OPEN processing - (BGP type code 1)
     -- (reset the UPDATE/NOTIFICATION timestamps if we see an OPEN)
-    f' ts 1 ( Flow tOpen _ _ _ ) = Flow (Just ts) Nothing Nothing Nothing
+    f' ts 1 ( Flow tOpen _ _ _ openCount _ ) = Flow (Just ts) Nothing Nothing Nothing (openCount+1) 0
 
     -- UPDATE processing - (BGP type code 2)
-    f' ts 2 ( Flow tOpen Nothing Nothing tNotify ) = Flow tOpen (Just ts) (Just ts) tNotify
-    f' ts 2 ( Flow tOpen tFirstUpdate tLastUpdate tNotify ) = Flow tOpen tFirstUpdate (Just ts) tNotify
+    f' ts 2 ( Flow tOpen Nothing Nothing tNotify updateCount openCount ) = Flow tOpen (Just ts) (Just ts) tNotify (updateCount+1) openCount 
+    f' ts 2 ( Flow tOpen tFirstUpdate tLastUpdate tNotify updateCount openCount ) = Flow tOpen tFirstUpdate (Just ts) tNotify (updateCount+1) openCount 
 
     -- NOTIFICATION processing - (BGP type code 3)
-    f' ts 3 ( Flow tOpen tFirstUpdate tLastUpdate tNotify ) = Flow tOpen tFirstUpdate tLastUpdate (Just ts)
+    f' ts 3 ( Flow tOpen tFirstUpdate tLastUpdate tNotify updateCount openCount ) = Flow tOpen tFirstUpdate tLastUpdate (Just ts) updateCount openCount 
 
     -- other, e.g. KEEPALIVE processing - (BGP type code 4+)
     f' _ _ x = x
